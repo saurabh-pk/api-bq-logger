b658c1db2ab46ce438a1c8033c518d1274a69aae
Saurabh Kshirsagar <saurabh@prodiodesignworks.in>
2018-06-23 18:29:03 +0530

    table column added

--- index.js
+++ index.js
@@ -37,62 +37,72 @@
     function remoteActions(app) {
         app.remotes().before('**', (ctx, next) => {
             ctx.req['request_id'] = req_id(ctx.req.ip);
+            ctx.req['request_time'] = new Date().getTime();
             var json = {
-                api_url: ctx.req.baseUrl,
-                hostname: ctx.req.hostname,
-                ip_from: ctx.req.ip,
+                request_id: ctx.req.request_id,
+                requested_api_url: ctx.req.baseUrl,
+                requesting_hostname: ctx.req.hostname,
+                requesting_ip: ctx.req.ip,
                 request_method: ctx.req.method,
                 request_url: ctx.req.originalUrl,
-                request_id: ctx.req.request_id,
-                user_agent:ctx.req.headers['user-agent'],
-                referer:ctx.req.headers.referer,
-                params:JSON.stringify(ctx.req.params),
-                query:JSON.stringify(ctx.req.query),
-                body:JSON.stringify(ctx.req.body),
-                accessToken:ctx.req.accessToken,
-                args:JSON.stringify(ctx.args),
+                request_from_user_agent:ctx.req.headers['user-agent'],
+                request_referer:ctx.req.headers.referer,
+                request_params:JSON.stringify(ctx.req.params),
+                request_query:JSON.stringify(ctx.req.query),
+                request_body:JSON.stringify(ctx.req.body),
+                request_accessToken:ctx.req.accessToken,
+                request_args:JSON.stringify(ctx.args),
+                request_headers:JSON.stringify(ctx.req.headers),
+                processing_time:0,
                 action_type: "request",
-                action_info: ""
+                action_details: ""
             };
             insertInBigquery(json);
             next();
         });
         app.remotes().after('**', (ctx, next) => {
             var json = {
-                api_url: ctx.req.baseUrl,
-                hostname: ctx.req.hostname,
-                ip_from: ctx.req.ip,
+                request_id: ctx.req.request_id,
+                requested_api_url: ctx.req.baseUrl,
+                requesting_hostname: ctx.req.hostname,
+                requesting_ip: ctx.req.ip,
                 request_method: ctx.req.method,
                 request_url: ctx.req.originalUrl,
-                request_id: ctx.req.request_id,
-                user_agent:ctx.req.headers['user-agent'],
-                referer:ctx.req.headers.referer,
-                params:JSON.stringify(ctx.req.params),
-                query:JSON.stringify(ctx.req.query),
-                body:JSON.stringify(ctx.req.body),
-                accessToken:ctx.req.accessToken,
-                args:JSON.stringify(ctx.args),
+                request_from_user_agent:ctx.req.headers['user-agent'],
+                request_referer:ctx.req.headers.referer,
+                request_params:JSON.stringify(ctx.req.params),
+                request_query:JSON.stringify(ctx.req.query),
+                request_body:JSON.stringify(ctx.req.body),
+                request_accessToken:ctx.req.accessToken,
+                request_args:JSON.stringify(ctx.args),
+                request_headers:JSON.stringify(ctx.req.headers),
+                processing_time:(new Date().getTime() - ctx.req.request_time),
                 action_type: "response",
-                action_info: ""
+                action_details: ""
             };
+            if(ctx.result == null){
+                json.action_info = 'RESULT_IS_NULL';
+            }
             insertInBigquery(json);
             next();
         });
         app.remotes().afterError('**', (ctx, next) => {
             var json = {
-                api_url: ctx.req.baseUrl,
-                hostname: ctx.req.hostname,
-                ip_from: ctx.req.ip,
+                request_id: ctx.req.request_id,
+                requested_api_url: ctx.req.baseUrl,
+                requesting_hostname: ctx.req.hostname,
+                requesting_ip: ctx.req.ip,
                 request_method: ctx.req.method,
                 request_url: ctx.req.originalUrl,
-                request_id: ctx.req.request_id,
-                user_agent:ctx.req.headers['user-agent'],
-                referer:ctx.req.headers.referer,
-                params:JSON.stringify(ctx.req.params),
-                query:JSON.stringify(ctx.req.query),
-                body:JSON.stringify(ctx.req.body),
-                accessToken:ctx.req.accessToken,
-                args:JSON.stringify(ctx.args),
+                request_from_user_agent:ctx.req.headers['user-agent'],
+                request_referer:ctx.req.headers.referer,
+                request_params:JSON.stringify(ctx.req.params),
+                request_query:JSON.stringify(ctx.req.query),
+                request_body:JSON.stringify(ctx.req.body),
+                request_accessToken:ctx.req.accessToken,
+                request_args:JSON.stringify(ctx.args),
+                request_headers:JSON.stringify(ctx.req.headers),
+                processing_time:(new Date().getTime() - ctx.req.request_time),
                 action_type: "error",
                 action_info: ctx.error
             };
